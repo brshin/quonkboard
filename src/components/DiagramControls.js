55e9364fb13e4a67fba5da29105721a691b04faf
Brandon Shin <143565532+brshin@users.noreply.github.com>
2026-02-25 15:55:55 -0600

    update valves & pt, name

--- src/components/DiagramControls.js
+++ src/components/DiagramControls.js
@@ -59,15 +59,15 @@ class DiagramControls extends React.Component {
             <div style={{position: "relative"}}>
                 <img src={Diagram} alt="Diagram" style={{height: 350}}></img>
                 <Box sx={{textAlign: "center"}} id="diagram-box">
-                    <div id="feed_line_pt"><GaugeReading title="Feed Line PT" ref={this.gauge_refs.feed_line_pt}/></div>
+                    {/* <div id="feed_line_pt"><GaugeReading title="Feed Line PT" ref={this.gauge_refs.feed_line_pt}/></div>
                     <div id="cc_pt"><GaugeReading title="CC PT" ref={this.gauge_refs.cc_pt}/></div>
-                    <div id="injector_pt"><GaugeReading title="Inj. PT" ref={this.gauge_refs.injector_pt}/></div>
+        <div id="injector_pt"><GaugeReading title="Inj. PT" ref={this.gauge_refs.injector_pt}/></div> */}
                     <div id="ox_tank_pt"><GaugeReading title="Ox. PT" ref={this.gauge_refs.ox_tank_pt}/></div>
                     <div id="load_cell"><GaugeReading title="Load Cell" size={90} ref={this.gauge_refs.load_cell}/></div>
-                    <div id="ox_fill_valve"><Valve title="Oxidizer Fill Valve" toggle_cmd={this.sendDriverCommand("ox_fill")} ref={this.valve_refs.ox_fill}/></div>
-                    <div id="ground_vent_valve"><Valve title="Ground Vent Valve" toggle_cmd={this.sendDriverCommand("ground_vent")} opposite={true} ref={this.valve_refs.ground_vent} text_bottom text_margin="22px"/></div>
-                    <div id="nitrogen_purge_valve"><Valve title="Engine Vent Valve" toggle_cmd={this.sendDriverCommand("engine_vent")} ref={this.valve_refs.engine_vent}/></div>
-                    <div id="engine_isolation_valve"><Valve title="OPS Pneumatic Valve" toggle_cmd={this.sendDriverCommand("ops_pneumatic")} ref={this.valve_refs.ops_pneumatic}/></div>
+                    <div id="ox_fill_valve"><Valve title="Fill Valve" toggle_cmd={this.sendDriverCommand("ox_fill")} ref={this.valve_refs.ox_fill}/></div>
+                    <div id="ground_vent_valve"><Valve title="Vehicle Valve 1" toggle_cmd={this.sendDriverCommand("ground_vent")} opposite={true} ref={this.valve_refs.ground_vent} text_bottom text_margin="22px"/></div>
+                    <div id="nitrogen_purge_valve"><Valve title="Vehicle Valve 2" toggle_cmd={this.sendDriverCommand("engine_vent")} ref={this.valve_refs.engine_vent}/></div>
+                    <div id="engine_isolation_valve"><Valve title="Purge Valve" toggle_cmd={this.sendDriverCommand("ops_pneumatic")} ref={this.valve_refs.ops_pneumatic}/></div>
                 </Box>
             </div>
         )
